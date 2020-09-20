06d9f8682ed560af9c637b40350a4a030c6f1283
YouLanjie <66255345+YouLanjie@users.noreply.github.com>
2020-09-20 15:04:06 +0800

    Add files via upload

---
+++ head.h
@@ -0,0 +1,14 @@
+#include <stdio.h>                           //标准库
+#include <string.h>                          //strcmp();
+#include <stdlib.h>                          //system();srand();rand();
+#include <unistd.h>                          //sleep();
+#include <time.h>                            //time(NULL);
+
+void cls();
+void welcome();
+void game();
+void set();
+void help();
+
+int my,my_g,my_v=0,npc,npc_g,npc_v=0;
+int bf1,bf2;
