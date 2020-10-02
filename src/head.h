a20aaae9da92bc0d6f003c04c20d6f3e23c515d2
YouLanjie <66255345+YouLanjie@users.noreply.github.com>
2020-10-02 15:38:03 +0800

    Update head.h

--- src/head.h
+++ src/head.h
@@ -3,16 +3,17 @@
 #include <stdlib.h>                          //system();srand();rand();
 #include <unistd.h>                          //sleep();
 #include <time.h>                            //time(NULL);
-#include <sys/stat.h>
-struct data{
-	int x;
-	int x2;
-	int g;
-	int v;
-}my={500,500,0,0},npc={500,500,0,0};
-void cls();
-void welcome();
-void game();
-void set();
-void help();
-float size();
+#include <sys/stat.h>                        //Linux专用，用于获取文件字节数
+
+struct data{                                 //定义存放角色数据的结构体Data
+	int x;                               //血量
+	int x2;                              //备份的血量，用于战斗结束后恢复血量
+	int g;                               //攻击力
+	int v;                               //胜利的次数
+}my={500,500,0,0},npc={500,500,0,0};         //分别定义存放玩家和NPC数据的结构体函数
+void cls();                                  //清屏
+void welcome();                              //主菜单打印
+void game();                                 //游戏
+void sava();                                 //游戏记录
+void help();                                 //帮助
+float size();                                //获取字节数
