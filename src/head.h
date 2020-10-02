2ef4bdbc9c78b20263e3bee9589bf668617dc5dd
YouLanjie <66255345+YouLanjie@users.noreply.github.com>
2020-10-02 16:18:01 +0800

    Update head.h

--- src/head.h
+++ src/head.h
@@ -11,9 +11,10 @@ struct data{                                 //定义存放角色数据的结构
 	int g;                               //攻击力
 	int v;                               //胜利的次数
 }my={500,500,0,0},npc={500,500,0,0};         //分别定义存放玩家和NPC数据的结构体函数
+
 void cls();                                  //清屏
 void welcome();                              //主菜单打印
 void game();                                 //游戏
-void sava();                                 //游戏记录
+void save();                                 //游戏记录
 void help();                                 //帮助
 float size();                                //获取字节数
